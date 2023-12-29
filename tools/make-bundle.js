a07c39e9bbbdc237fa4b575e635a8ed690e5e045
Uri Shaked <uri@urishaked.com>
2023-12-29 15:17:09 +0200

    feat: board bundle
    automatically generate a bundle with all boards

---
+++ tools/make-bundle.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+const jsonc = require('jsonc-parser');
+const { execSync } = require('child_process');
+
+const boardsRoot = path.join(__dirname, '..', 'boards');
+const bundleFile = path.join(boardsRoot, 'bundle.json');
+
+const bundle = {};
+for (const boardId of fs.readdirSync(boardsRoot)) {
+  try {
+    const boardPath = path.join(boardsRoot, boardId);
+    const boardJson = jsonc.parse(fs.readFileSync(path.join(boardPath, 'board.json'), 'utf-8'));
+    const rev = execSync(`git log -1 --pretty=format:"%h" ${boardPath}`).toString().trim();
+
+    bundle[boardId] = { rev, def: boardJson, svg: fs.readFileSync(path.join(boardPath, 'board.svg'), 'utf-8') };
+  } catch (err) {
+    console.error(`Error reading ${boardId}: ${err}`);
+  }
+}
+fs.writeFileSync(bundleFile, JSON.stringify(bundle));
