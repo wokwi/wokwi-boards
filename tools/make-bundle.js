0cb89a93a28a54aa8851abea98c099fec05dba8d
Uri Shaked <uri@urishaked.com>
2026-01-29 09:30:36 +0000

    feat(bundle): add boards.json without SVGs

--- tools/make-bundle.js
+++ tools/make-bundle.js
@@ -5,8 +5,10 @@ const { execSync } = require('child_process');
 
 const boardsRoot = path.join(__dirname, '..', 'boards');
 const bundleFile = path.join(boardsRoot, 'bundle.json');
+const boardsFile = path.join(boardsRoot, 'boards.json');
 
 const bundle = {};
+const boards = {};
 for (const boardId of fs.readdirSync(boardsRoot)) {
   try {
     const boardPath = path.join(boardsRoot, boardId);
@@ -14,8 +16,10 @@ for (const boardId of fs.readdirSync(boardsRoot)) {
     const rev = execSync(`git log -1 --pretty=format:"%h" ${boardPath}`).toString().trim();
 
     bundle[boardId] = { rev, def: boardJson, svg: fs.readFileSync(path.join(boardPath, 'board.svg'), 'utf-8') };
+    boards[boardId] = { rev, def: boardJson };
   } catch (err) {
     console.error(`Error reading ${boardId}: ${err}`);
   }
 }
 fs.writeFileSync(bundleFile, JSON.stringify(bundle));
+fs.writeFileSync(boardsFile, JSON.stringify(boards));
